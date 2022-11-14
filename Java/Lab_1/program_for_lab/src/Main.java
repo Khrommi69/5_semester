a80af50db3e27d599a848401e365bb60dac6c17c
Khrommi69 <danilkhromdewil@gmail.com>
2022-11-14 13:08:39 +0300

    Сделал прогу к первой лабе по Java

--- Java/Lab_1/program_for_lab/src/Main.java
+++ Java/Lab_1/program_for_lab/src/Main.java
@@ -1,23 +1,77 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Scanner; //для чтения всякого
+import java.io.*; //для работы с файлами
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+        System.out.println("Программа находит все положительные степени двойки,значение которых не превышает величины введенного числа.");
+        int number = 0;
 
+        //Если в cmd были переданы аргументы вывести их на экран
+        if (args.length > 0) {
+            System.out.print("Аргументы коммандной строки на входе:");
+            System.out.println(Arrays.toString(args));
+        }
 
+        //Проверка на наличие аргументов коммандной строки
+        String inputFile = "";
+        String outputFile = "";
+        for (int i = 0; i < args.length; i++) {
+            //если есть флаг -i то следующий аргумент коммандной строки это название input файла
+            if (args[i].equals("-i"))
+                inputFile = args[++i];
+            //если есть флаг -o то следующий аргумент коммандной строки это название output файла
+            else if (args[i].equals("-o"))
+                outputFile = args[++i];
+        }
 
+        //если есть флаг -i попытка открыть файл с входным значением
+        if (!inputFile.isEmpty()) {
+            try {
+                //для обращения к input файлу
+                BufferedReader fileIn = new BufferedReader(new FileReader(inputFile));
+                //превращение строки из файла в число
+                number = Integer.parseInt(fileIn.readLine().trim());
+                fileIn.close();
+            }
+            catch (IOException e) {
+                System.err.println("Ошибка при работе с input файлом " + e);
+            }
+            catch (NumberFormatException e) {
+                System.err.println("Ошибка преобразования входных данных " + e);
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Введите число вручную >> ");
+                number = scan.nextInt();
+            }
+        }
+        else {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Введите число >> ");
+            number = scan.nextInt();
+        }
 
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("РџСЂРѕРіСЂР°РјРјР° РЅР°С…РѕРґРёС‚ РІСЃРµ РїРѕР»РѕР¶РёС‚РµР»СЊРЅС‹Рµ СЃС‚РµРїРµРЅРё РґРІРѕР№РєРё,\nР·РЅР°С‡РµРЅРёРµ РєРѕС‚РѕСЂС‹С… РЅРµ РїСЂРµРІС‹С€Р°РµС‚ РІРµР»РёС‡РёРЅС‹ РІРІРµРґРµРЅРЅРѕРіРѕ С‡РёСЃР»Р°.");
-
-        System.out.print("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ >> ");
-        int number = scan.nextInt();
-        System.out.println("Р’РІРµРґС‘РЅРЅРѕРµ С‡РёСЃР»Рѕ >> " + number);
-        for (int i = 0; Math.pow(2, i) <= number; i++) {
-            System.out.println("2^" + i + " = " + Math.pow(2, i));
+        //если есть флаг -o попытка открыть файл для вывода результатов
+        if (!outputFile.isEmpty()) {
+            try {
+                //вывод в файл результатов работы программы
+                PrintWriter pw = new PrintWriter(outputFile);
+                pw.println("Программа находит все положительные степени двойки,значение которых не превышает величины введенного числа.");
+                pw.println("Введённое число >> " + number);
+                for (int i = 0; Math.pow(2, i) <= number; i++) {
+                    pw.println("2^" + i + " = " + Math.pow(2, i));
+                }
+                pw.close();
+                System.out.println("Результаты работы программы выведены в файл");
+            } catch (IOException e) {
+                System.err.println("Ошибка при работе с output файлом " + e);
+            }
+        }
+        else {
+            System.out.println("Введённое число >> " + number);
+            for (int i = 0; Math.pow(2, i) <= number; i++) {
+                System.out.println("2^" + i + " = " + Math.pow(2, i));
+            }
         }
     }
 
