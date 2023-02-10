a43acb1c18b8148cea6706e259298dcec2323f4e
Khrommi69 <danilkhromdewil@gmail.com>
2023-02-10 12:08:09 +0300

    что то делал с 3 лабой по ТСИС

---
+++ Technical means of information systems/LR3/program.asm
@@ -0,0 +1,34 @@
+org 100h
+
+CALL main
+RET
+
+.code
+main PROC
+    MOV AX, 257                     ;+4
+    MOV BX, 0                       ;+4
+    MOV CX, -257                    ;+4 
+    
+    CALL f                          ;+19
+    RET
+main ENDP
+
+f PROC
+    MOV f_c, CX                            ;+10
+    SAL AX, 1    ;AX=2*a                   ;+2
+    SUB CX, AX   ;CX=c-2*a                 ;+4
+    MOV CX, 3    ;move result to AX        ;+2
+                 ;AX=AX*CX, where CX=3
+    IMUL CX                                ;+4
+    SUB BX, f_c  ;b=b-c                    ;+(128-154)
+    INC BX       ;b=b+1                    ;+16
+    SAR BX, 1    ;b=b/2                    ;+2
+    ADD AX, BX                             ;+2
+    MOV BX, 0                              ;+3
+    MOV CX, 0                              ;+4
+                                           ;+4
+    RET
+f END
+
+.data
+    f_c dw ?
