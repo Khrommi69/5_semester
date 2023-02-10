1e3ecd57f63fbd79f6765be559bf121fb6483ebc
Danil <danilkhromdewil@gmail.com>
2023-02-10 20:21:08 +0300

    Всё 3 лаба готова больше не прикоснусь

--- Technical means of information systems/LR3/program.asm
+++ Technical means of information systems/LR3/program.asm
@@ -1,32 +1,33 @@
-org 100h
+org 100h    
 
-CALL main
+CALL main                          
 RET
 
 .code
 main PROC
-    MOV AX, 257                     ;+4
-    MOV BX, 0                       ;+4
-    MOV CX, -257                    ;+4 
+    MOV AX, 10             ; + 4
+    MOV BX, 100            ; + 4
+    MOV CX, 10             ; + 4
     
-    CALL f                          ;+19
-    RET
+    CALL f                 ; + 19
+    RET     
 main ENDP
 
 f PROC
-    MOV f_c, CX                            ;+10
-    SAL AX, 1    ;AX=2*a                   ;+2
-    SUB CX, AX   ;CX=c-2*a                 ;+4
-    MOV CX, 3    ;move result to AX        ;+2
-                 ;AX=AX*CX, where CX=3
-    IMUL CX                                ;+4
-    SUB BX, f_c  ;b=b-c                    ;+(128-154)
-    INC BX       ;b=b+1                    ;+16
-    SAR BX, 1    ;b=b/2                    ;+2
-    ADD AX, BX                             ;+2
-    MOV BX, 0                              ;+3
-    MOV CX, 0                              ;+4
-                                           ;+4
+    MOV f_c, CX                          ; + 10
+    SAL AX, 1     ;AX=2*a                ; + 2
+    SUB CX, AX    ;CX=c-2*a              ; + 4
+    MOV AX, CX    ;move result to AX     ; + 2
+                  ;AX=AX*CX, where CX = 3
+    MOV CX, 3                            ; + 4
+    IMUL CX                              ; + (128~154)
+    SUB BX, f_c    ;b-=c                 ; + 16
+    INC BX         ;++b                  ; + 2
+    SAR BX, 1      ;b/=2                 ; + 2
+    ADD AX, BX                           ; + 3
+    MOV BX, 0                            ; + 4
+    MOV CX, 0                            ; + 4
+    
     RET
 f ENDP
 
